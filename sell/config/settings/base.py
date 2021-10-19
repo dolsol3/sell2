e93f93dd76c70678b00588662d881966bd224971
dolsol3 <dolsol4@gmail.com>
2021-10-20 00:57:25 +0900

    first commit
    postgres setting
    django setting
    virtual

--- sell/config/settings/base.py
+++ sell/config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres://localhost/sell"),
+    "default": env.db("DATABASE_URL", default="postgres://postgres:1003@localhost:5432/selldb"),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
